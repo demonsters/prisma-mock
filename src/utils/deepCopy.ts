2b68746fc6d0f07101afee0387104d2c61dcdc87
HANAI, Tohru <tohru@reedom.com>
2025-03-22 11:44:33 +0900

    fix: handle Date instances in deepCopy function

--- src/utils/deepCopy.ts
+++ src/utils/deepCopy.ts
@@ -6,6 +6,9 @@ export function deepCopy<T>(source:T): T {
   } else if (Array.isArray(source))
   {
     return source.map(deepCopy) as T
+  } else if (source instanceof Date)
+  {
+    return new Date(source) as unknown as T
   }
 
   return Object.fromEntries(Object.entries(source).map(([k,v]) => ([k, deepCopy(v)]))) as T
