072564a5d44c3de68ea3f7faaab840578d8cd6b0
Stijn <stijn@demonsters.com>
2025-11-11 11:42:22 +0100

    Refactor createPrismaClient to enhance type safety by allowing generic type parameters for PrismaClient.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Prisma, PrismaClient } from "@prisma/client/default"
 import { MockPrismaOptions } from "./types"
 import createPrismaMock from "./client"
 
-export default function createPrismaClient(options?: MockPrismaOptions<PrismaClient, typeof Prisma>) {
-  return createPrismaMock<PrismaClient, typeof Prisma>(Prisma, options)
+export default function createPrismaClient<P extends PrismaClient = PrismaClient>(options?: MockPrismaOptions<P, typeof Prisma>) {
+  return createPrismaMock<P, typeof Prisma>(Prisma, options)
 }
